aea24b4a87dcff1746cbd671b9bc5eda216158fa
Matthew Smith <mattsheridansmith@gmail.com>
2017-11-12 11:28:29 -0500

    Small tweaks to set up internal contract time check. Feature in progress.

--- contracts/Capsule.sol
+++ contracts/Capsule.sol
@@ -2,12 +2,26 @@ pragma solidity ^0.4.2;
 
 contract Capsule {
   mapping (address => bytes32) messages;
+  mapping (address => uint) timestamps;
 
   function setMessage(bytes32 message) public {
     messages[msg.sender] = message;
+    timestamps[msg.sender] = block.timestamp;
   }
 
-  function getMessage() constant returns(bytes32) {
-    return messages[msg.sender];
+  function getMessage() constant returns(bytes32 message, uint nope) {
+    uint diff = getTimeDifference();
+    message = "Not yet";
+    if (diff > 1000000) {
+      message = messages[msg.sender];
+    }
+  }
+
+  function getTimeStamp() constant returns(uint time) {
+    return timestamps[msg.sender];
+  }
+
+  function getTimeDifference() constant returns(uint timeDiff) {
+    return now - timestamps[msg.sender];
   }
 }
